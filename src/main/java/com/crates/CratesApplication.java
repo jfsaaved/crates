ee21574338fd242c09c894baa43bf90592e78a57
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2018-09-09 12:33:08 -0400

    First commit

---
+++ src/main/java/com/crates/CratesApplication.java
@@ -0,0 +1,12 @@
+package com.crates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CratesApplication.class, args);
+	}
+}
