ea3ed96a71642c26839bb5372502d1c1fde295e8
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2018-09-11 15:42:20 -0400

    2nd commit

---
+++ src/main/java/com/crates/domain/Sample.java
@@ -0,0 +1,11 @@
+package com.crates.domain;
+
+public class Sample {
+	
+	private String link;
+	
+	public Sample(){
+		this.link = "";
+	}
+
+}
---
+++ src/main/java/com/crates/domain/User.java
@@ -0,0 +1,5 @@
+package com.crates.domain;
+
+public class User {
+
+}
