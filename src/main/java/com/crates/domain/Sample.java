82721d92c63b43e0e43d21e1a78e80faa5edb6ef
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2018-09-25 14:23:58 -0400

    Sample object

--- src/main/java/com/crates/domain/Sample.java
+++ src/main/java/com/crates/domain/Sample.java
@@ -10,9 +10,35 @@ public class Sample {
 	private Long id;
 	
 	private String link;
+	private String title;
+	private String artist;
 	
 	public Sample(){
-		this.link = "";
+		this.setLink("");
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
 	}
 
 }
