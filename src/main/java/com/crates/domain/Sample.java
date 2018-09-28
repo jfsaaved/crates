60db08a73e5e206181cc7d1271ac6d63faa51e83
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2018-09-28 13:50:35 -0400

    Making use of Lombok

--- src/main/java/com/crates/domain/Sample.java
+++ src/main/java/com/crates/domain/Sample.java
@@ -3,42 +3,24 @@ package com.crates.domain;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Sample {
 	
 	@Id
 	@GeneratedValue
 	private Long id;
 	
-	private String link;
-	private String title;
-	private String artist;
+	private @Getter @Setter String link;
+	private @Getter @Setter String title;
+	private @Getter @Setter String artist;
 	
 	public Sample(){
-		this.setLink("");
+		this.link = "";
+		this.artist = "";
+		this.title = "";
 	}
 
-	public String getLink() {
-		return link;
-	}
-
-	public void setLink(String link) {
-		this.link = link;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getArtist() {
-		return artist;
-	}
-
-	public void setArtist(String artist) {
-		this.artist = artist;
-	}
 
 }
