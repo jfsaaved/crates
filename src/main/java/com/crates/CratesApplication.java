46aafe6d6bd70e5ab3197641bede0602d5c003e8
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2018-10-02 16:47:12 -0400

    YAML configuration

--- src/main/java/com/crates/CratesApplication.java
+++ src/main/java/com/crates/CratesApplication.java
@@ -1,12 +1,23 @@
 package com.crates;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CratesApplication {
 
+    @Autowired
+    private YAMLConfig myConfig;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CratesApplication.class, args);
+
 	}
+	
+    public void run(String... args) throws Exception {
+        System.out.println("using environment: " + myConfig.getEnvironment());
+        System.out.println("name: " + myConfig.getName());
+        System.out.println("servers: " + myConfig.getServers());
+    }
 }
