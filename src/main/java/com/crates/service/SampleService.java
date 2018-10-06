24577197d88c58f28ad1bdf244864139a1d0d35e
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2018-10-06 08:29:02 -0400

    Redoing the database

---
+++ src/main/java/com/crates/service/SampleService.java
@@ -0,0 +1,12 @@
+package com.crates.service;
+
+import java.util.Optional;
+
+import com.crates.domain.Sample;
+import com.crates.repository.*;
+
+public class SampleService {
+
+	private SampleRepository sampleRepositry;
+	
+}
