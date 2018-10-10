3546bf10d79cf9410db4fc7cd1c571519fd979ef
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2018-10-10 13:38:46 -0400

    Populating Element.java

--- src/main/java/com/crates/domain/Element.java
+++ src/main/java/com/crates/domain/Element.java
@@ -1,5 +1,32 @@
 package com.crates.domain;
 
-public class Element {
+import java.util.List;
 
+public class Element {
+	
+	private String name;
+	private List<String> alternatives;
+	
+	public Element(){
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getAlternatives() {
+		return alternatives;
+	}
+
+	public void setAlternatives(List<String> alternatives) {
+		this.alternatives = alternatives;
+	}
+	
+
+	
 }
