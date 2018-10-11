5bd0c581f2a216c3dc2d80e64cd779cfc1d366b7
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2018-10-11 14:17:09 -0400

    Populating ElementService

--- src/main/java/com/crates/service/ElementService.java
+++ src/main/java/com/crates/service/ElementService.java
@@ -1,5 +1,20 @@
 package com.crates.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.crates.repository.ElementRepository;
+import com.crates.repository.SampleRepository;
+
 public class ElementService {
+	
+	private ElementRepository elementRepository;
+	
+	@Autowired
+	public ElementService(ElementRepository elementRepository){
+		super();
+		this.elementRepository = elementRepository;
+	}
+	
+	
 
 }
