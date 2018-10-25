1f54a8a4ef033b05f25f70159b62409100f9f509
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2018-10-25 13:12:37 -0400

    Roles and user, removing producer class

---
+++ src/main/java/com/crates/domain/User.java
@@ -0,0 +1,61 @@
+package com.crates.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String username;
+	private String password;
+	private String passwordConfirm;
+	private Set<Role> roles;
+	
+	//Producer stuff
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	
+	
+
+}
