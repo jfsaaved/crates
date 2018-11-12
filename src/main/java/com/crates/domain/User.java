8915231600f7910a3117bfb35a29307d690b4e37
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2018-11-12 14:59:03 -0500

    Time to sleep

--- src/main/java/com/crates/domain/User.java
+++ src/main/java/com/crates/domain/User.java
@@ -13,8 +13,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String username;
-	private String password;
-	private String passwordConfirm;
 	
 	@OneToMany(targetEntity=Flip.class, mappedBy="user", fetch=FetchType.EAGER)
 	private List<Flip> flips;
@@ -30,22 +28,5 @@ public class User {
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getPasswordConfirm() {
-		return passwordConfirm;
-	}
-
-	public void setPasswordConfirm(String passwordConfirm) {
-		this.passwordConfirm = passwordConfirm;
-	}
-	
 
 }
